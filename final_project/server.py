7aac8f9b6d9cbf273ab235325b3fb8a91652bdfe
https://github.com/Edidiongumoren <edidiongumoreno6@gmail.com>
2023-06-05 16:38:29 -0400

    Add file

--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,15 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    # Write your code here translation
+    translation = translate_english_to_french(textToTranslate)
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
+    translation = translate_french_to_english(textToTranslate)
     return "Translated text to English"
 
 @app.route("/")
